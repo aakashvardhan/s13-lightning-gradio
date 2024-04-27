a58f1a7981e99059e737081fad5c449c8a1bd2d9
aakashv100 <vardhan.aakash1@gmail.com>
2024-04-27 10:50:37 +0530

    Update num_epochs in config.py

--- config.py
+++ config.py
@@ -12,9 +12,9 @@ def get_config():
 
     config = {
         "device": torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+        "num_epochs": 20,
         "num_workers": 2,
         "batch_size": 128,
-        "num_epochs": 20,
         "precision": 16,
         "progress_bar_refresh_rate": 10,
         "lr": 0.01,
