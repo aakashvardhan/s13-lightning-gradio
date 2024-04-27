72d11368585909a78cae835c920d72b1b5f0a551
aakashv100 <vardhan.aakash1@gmail.com>
2024-04-27 11:13:19 +0530

    Update learning rate configuration in config.py

--- config.py
+++ config.py
@@ -18,14 +18,12 @@ def get_config():
         "precision": "16-mixed",
         "accelerator": "cuda",
         "progress_bar_refresh_rate": 10,
-        "lr": 0.01,
         "model_folder": "weights",
         "model_basename": "model_",
         "preload": True,
         "experiment_name": "runs/model",
         "end_lr": 10,
         "num_iter": 100,
-        "best_lr": 0.01,
         "ckpt": False,
         "ckpt_path": ""
     }
